b8e92ffa039d8c681f1679ee414f7a3db9a48a88
Hamidah2006 <hamidaholaniyi@gmail.com>
2025-07-23 10:30:05 +0100

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-sky-600 text-white p-2 text-center">
+    &copy; 2025 HealthWise. All rights reserved.
+  </footer>
+);
+
+export default Footer;
